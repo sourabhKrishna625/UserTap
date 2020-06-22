397e5044595fe513cf2d5dcf586cd6a32bb06c6e
sourabKrishna625 <sourabhjosyula@gmail.com>
2020-06-22 00:29:07 -0400

    UAV-Sync-2
    Errors update

--- app/src/main/java/com/seventhheaven/usertap/UI/Activities/MainActivity.kt
+++ app/src/main/java/com/seventhheaven/usertap/UI/Activities/MainActivity.kt
@@ -12,8 +12,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var offersBlankFragment: OffersBlankFragment
     lateinit var aboutUsBlankFragment: AboutUsBlankFragment
     //private lateinit var mPager:ViewPager
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
